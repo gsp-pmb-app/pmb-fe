b0fb28f1520e08d08957da5863ea20b31c34508d
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2026-02-03 22:46:46 +0700

    first commit - base project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-rose-400">
+      <h1 className="text-4xl font-bold text-white">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
